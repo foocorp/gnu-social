4f2d3b733a9095cf90591b1a9857dc735438fd62
Matt Lee <mattl@cnuk.org>
2010-11-29 15:16:24 -0500

    updated template

--- tpl/social.php
+++ tpl/social.php
@@ -13,7 +13,7 @@
         <?php echo section('head'); ?>
         </head>
     <body id="<?php echo section('action'); ?>">
-        <div id="custom-doc" class="yui-t2">
+        <div id="doc2" class="yui-t6">
            <div id="hd">
                 <h1><a href="/">GNU social</a></h1>            
                 <?php echo section('nav'); ?>
@@ -21,32 +21,26 @@
             <div id="bd">
                 <div id="yui-main">
                     <div class="yui-b" id="social">
-                        <div class="yui-gc">
-                            <div class="yui-u first">
+                        <div class="yui-g">
                                 <?php echo section('noticeform'); ?>
                                 <?php echo section('bodytext'); ?>
-
                             </div>
+</div>
+</div>
 
 
-                            <div class="yui-u" id="right-nav">
+                            <div class="yui-b" id="right-nav">
                                 <div id="aside_primary" class="aside">
                                     <?php echo section('subscriptions'); ?>
                                     <?php echo section('subscribers'); ?>
                                     <?php echo section('groups'); ?>
                                     <?php echo section('cloud'); ?>
                                     <?php echo section('popular'); ?>
+                                    <?php echo section('localnav'); ?>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="yui-b" id="sidebar">
-                    <?php echo section('localnav'); ?>
-                </div>
-            </div>
             <div id="ft">
-							 <p>This is <a href="http://www.gnu.org/software/social">GNU social</a> &mdash; licensed under the <a href="http://www.gnu.org/licenses/agpl-3.0.html">GNU Affero General Public License</a> version 3.0 or later. <a href="http://gitorious.org/+socialites/statusnet/gnu-social">Get the code</a>.</p>
+	      <p>This is <a href="http://www.gnu.org/software/social">GNU social</a> &mdash; licensed under the <a href="http://www.gnu.org/licenses/agpl-3.0.html">GNU Affero General Public License</a> version 3.0 or later. <a href="http://gitorious.org/+socialites/statusnet/gnu-social">Get the code</a>.</p>
             </div>
         </div>
     </body>
