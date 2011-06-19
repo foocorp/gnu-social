9272fcd3ab240b4242fdad75254be7f4ca641374
Siebrand Mazeland <s.mazeland@xs4all.nl>
2011-06-19 11:54:10 +0200

    Add translator documentation.

--- plugins/TabFocus/TabFocusPlugin.php
+++ plugins/TabFocus/TabFocusPlugin.php
@@ -51,6 +51,7 @@ class TabFocusPlugin extends Plugin
                             'author' => 'Craig Andrews and Paul Irish',
                             'homepage' => 'http://status.net/wiki/Plugin:TabFocus',
                             'rawdescription' =>
+                            // TRANS: Plugin description.
                             _m('TabFocus changes the notice form behavior so that, while in the text area, pressing the tab key focuses the "Send" button, matching the behavior of Twitter.'));
         return true;
     }
