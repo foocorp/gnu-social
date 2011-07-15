c579bcae7b9f42738fa3880c0998420bac7d5fae
Evan Prodromou <evan@status.net>
2011-07-15 12:49:52 -0400

    conversation API

--- lib/router.php
+++ lib/router.php
@@ -774,6 +774,11 @@ class Router
                         array('action' => 'ApiGroupProfileUpdate',
                               'id' => '[a-zA-Z0-9]+',
                               'format' => '(xml|json)'));
+                              
+            $m->connect('api/statusnet/conversation/:id.:format',
+                        array('action' => 'apiconversation',
+                              'id' => '[0-9]+',
+                              'format' => '(xml|json|rss|atom|as)'));
 
             // Lists (people tags)
 
