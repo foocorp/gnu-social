6319003a34a72255ea22e870367166d1d25ecb96
Siebrand Mazeland <s.mazeland@xs4all.nl>
2011-08-15 15:57:36 +0200

    Change "stream" to "timeline" for consistency.

--- actions/public.php
+++ actions/public.php
@@ -95,7 +95,7 @@ class PublicAction extends Action
 
         if (!$this->notice) {
             // TRANS: Server error displayed when a public timeline cannot be retrieved.
-            $this->serverError(_('Could not retrieve public stream.'));
+            $this->serverError(_('Could not retrieve public timeline.'));
             return;
         }
 
@@ -166,20 +166,20 @@ class PublicAction extends Action
                               common_local_url('ApiTimelinePublic',
                                                array('format' => 'as')),
                               // TRANS: Link description for public timeline feed.
-                              _('Public Stream Feed (Activity Streams JSON)')),
+                              _('Public Timeline Feed (Activity Streams JSON)')),
                     new Feed(Feed::RSS1, common_local_url('publicrss'),
                               // TRANS: Link description for public timeline feed.
-                              _('Public Stream Feed (RSS 1.0)')),
+                              _('Public Timeline Feed (RSS 1.0)')),
                      new Feed(Feed::RSS2,
                               common_local_url('ApiTimelinePublic',
                                                array('format' => 'rss')),
                               // TRANS: Link description for public timeline feed.
-                              _('Public Stream Feed (RSS 2.0)')),
+                              _('Public Timeline Feed (RSS 2.0)')),
                      new Feed(Feed::ATOM,
                               common_local_url('ApiTimelinePublic',
                                                array('format' => 'atom')),
                               // TRANS: Link description for public timeline feed.
-                              _('Public Stream Feed (Atom)')));
+                              _('Public Timeline Feed (Atom)')));
     }
 
     function showEmptyList()
