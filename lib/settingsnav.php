1f7f0690b3527577efa5f468ce8416a2cde380d2
Luke Fitzgerald <lw.fitzgerald@googlemail.com>
2011-09-20 02:45:02 +0100

    Correct ImPlugin to XmppPlugin

--- lib/settingsnav.php
+++ lib/settingsnav.php
@@ -121,7 +121,7 @@ class SettingsNav extends Menu
 
             Event::handle('EndAccountSettingsNav', array(&$this->action));
 
-            if (class_exists('ImPlugin') || class_exists('IrcPlugin') || class_exists('MsnPlugin')) {
+            if (class_exists('XmppPlugin') || class_exists('IrcPlugin') || class_exists('MsnPlugin')) {
                 $this->action->menuItem(common_local_url('imsettings'),
                                         // TRANS: Menu item in settings navigation panel.
                                         _m('MENU','IM'),
