ce044c40fb0c05d8f7869c8c06174b2c7428052b
Evan Prodromou <evan@status.net>
2011-09-27 09:42:34 -0400

    rewrite Profile_tag::getTagsArray() so it doesn't use joinAdd()

--- classes/Profile_tag.php
+++ classes/Profile_tag.php
@@ -99,23 +99,26 @@ class Profile_tag extends Managed_DataObject
     static function getTagsArray($tagger, $tagged, $auth_user_id=null)
     {
         $ptag = new Profile_tag();
-        $ptag->tagger = $tagger;
-        $ptag->tagged = $tagged;
 
-        if ($tagger != $auth_user_id) {
-            $list = new Profile_list();
-            $list->private = false;
-            $ptag->joinAdd($list);
-            $ptag->selectAdd();
-            $ptag->selectAdd('profile_tag.tag');
+        $qry = sprint('select profile_tag.tag '.
+                      'from profile_tag join profile_list '.
+                      ' on (profile_tag.tagger = profile_list.tagger ' .
+                      '     and profile_tag.tag = profile_list.tag) ' .
+                      'where profile_tag.tagger = %d ' .
+                      'and   profile_tag.tagged = %d ',
+                      $tagger, $tagged);
+
+        if ($auth_user_id != $tagger) {
+            $qry .= 'and profile_list.private = 0';
         }
 
         $tags = array();
-        $ptag->find();
+
+        $ptag->query($sql);
+
         while ($ptag->fetch()) {
             $tags[] = $ptag->tag;
         }
-        $ptag->free();
 
         return $tags;
     }
