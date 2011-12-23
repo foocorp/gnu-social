1757a653f8cdca75c4a7991e956b1f5289b54806
Brian Tegtmeier <btegtmeier@gmail.com>
2011-12-23 14:56:55 -0500

    Shortcut for (document).ready(). Changed .attr() to .prop() for checked and disabled.

--- js/install.js
+++ js/install.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
+$(function() {
     $.ajax({url:'check-fancy',
         type:'GET',
         success:function(data, textStatus) {
-            $('#fancy-enable').attr('checked', true);
-            $('#fancy-disable').attr('checked', false);
+            $('#fancy-enable').prop('checked', true);
+            $('#fancy-disable').prop('checked', false);
             $('#fancy-form_guide').text(data);
         },
         error:function(XMLHttpRequest, textStatus, errorThrown) {
-            $('#fancy-enable').attr('checked', false);
-            $('#fancy-disable').attr('checked', true);
-            $('#fancy-enable').attr('disabled', true);
-            $('#fancy-disable').attr('disabled', true);
+            $('#fancy-enable').prop('checked', false);
+            $('#fancy-disable').prop('checked', true);
+            $('#fancy-enable').prop('disabled', true);
+            $('#fancy-disable').prop('disabled', true);
             $('#fancy-form_guide').text("Fancy URL support detection failed, disabling this option. Make sure you renamed htaccess.sample to .htaccess.");
         }
     });
