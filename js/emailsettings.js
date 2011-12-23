af4f2a18c8bbfe9ce29f900fe00d5cfe99b31d2d
Brian Tegtmeier <btegtmeier@gmail.com>
2011-12-23 14:54:58 -0500

    Changed .attr() to .prop() for checked and disabled. Removed "style" removal which I assume was tied to opacity setting on line 9. Replaced "style" setting via attr() on line 12 with css().

--- js/emailsettings.js
+++ js/emailsettings.js
@@ -1,16 +1,16 @@
 $(function() {
 
 function toggleIncomingOptions() {
-    var enabled = $('#emailpost').attr('checked');
+    var enabled = $('#emailpost').prop('checked', true);
     if (enabled) {
         // Note: button style currently does not respond to disabled in our main themes.
         // Graying out the whole section with a 50% transparency will do for now. :)
         // @todo: add a general 'disabled' class style to the base themes.
-        $('#emailincoming').removeAttr('style')
-                           .find('input').removeAttr('disabled');
+        $('#emailincoming').css('opacity', '')
+                           .find('input').prop('disabled', false);
     } else {
-        $('#emailincoming').attr('style', 'opacity: 0.5')
-                           .find('input').attr('disabled', 'disabled');
+        $('#emailincoming').css('opacity', '0.5')
+                           .find('input').prop('disabled', true);
     }
 }
 
