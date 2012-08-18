d6cf6e88cd6533ffc33f559be5e2aa553b771625
Mikael Nordfeldth <mmn@hethane.se>
2012-08-18 09:56:38 +0200

    letting the noticeform at the top show, to fix broken reply button javascript

--- lib/searchaction.php
+++ lib/searchaction.php
@@ -84,10 +84,6 @@ class SearchAction extends Action
         return null;
     }
 
-    function showNoticeForm() {
-        // remote post notice form
-    }
-
     function showContent() {
         $this->showTop();
         $this->showForm();
