15d466ebe6576aa34efa7d4b5bb008e0d2cbc116
Evan Prodromou <evan@e14n.com>
2013-06-04 19:52:38 -0400

    Don't add content as title for notes

--- classes/Notice.php
+++ classes/Notice.php
@@ -1508,7 +1508,6 @@ class Notice extends Managed_DataObject
             $act->time    = strtotime($this->created);
             $act->link    = $this->bestUrl();
             $act->content = common_xml_safe_str($this->rendered);
-            $act->title   = common_xml_safe_str($this->content);
 
             $profile = $this->getProfile();
 
