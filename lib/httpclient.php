792e1aeb47749b576f99c52eb0b1c54ec27e6e2c
Mikael Nordfeldth <mmn@hethane.se>
2013-09-19 16:11:04 +0200

    StatusNet_HTTPResponse now prefixed with GNUSocial_

--- lib/httpclient.php
+++ lib/httpclient.php
@@ -54,7 +54,7 @@ require_once 'HTTP/Request2/Response.php';
  * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
  * @link http://status.net/
  */
-class StatusNet_HTTPResponse extends HTTP_Request2_Response
+class GNUSocial_HTTPResponse extends HTTP_Request2_Response
 {
     function __construct(HTTP_Request2_Response $response, $url, $redirects=0)
     {
@@ -173,7 +173,7 @@ class HTTPClient extends HTTP_Request2
     /**
      * Convenience function to run a GET request.
      *
-     * @return StatusNet_HTTPResponse
+     * @return GNUSocial_HTTPResponse
      * @throws HTTP_Request2_Exception
      */
     public function get($url, $headers=array())
@@ -184,7 +184,7 @@ class HTTPClient extends HTTP_Request2
     /**
      * Convenience function to run a HEAD request.
      *
-     * @return StatusNet_HTTPResponse
+     * @return GNUSocial_HTTPResponse
      * @throws HTTP_Request2_Exception
      */
     public function head($url, $headers=array())
@@ -198,7 +198,7 @@ class HTTPClient extends HTTP_Request2
      * @param string $url
      * @param array $headers optional associative array of HTTP headers
      * @param array $data optional associative array or blob of form data to submit
-     * @return StatusNet_HTTPResponse
+     * @return GNUSocial_HTTPResponse
      * @throws HTTP_Request2_Exception
      */
     public function post($url, $headers=array(), $data=array())
@@ -210,7 +210,7 @@ class HTTPClient extends HTTP_Request2
     }
 
     /**
-     * @return StatusNet_HTTPResponse
+     * @return GNUSocial_HTTPResponse
      * @throws HTTP_Request2_Exception
      */
     protected function doRequest($url, $method, $headers)
@@ -249,16 +249,16 @@ class HTTPClient extends HTTP_Request2
      */
     function userAgent()
     {
-        return "StatusNet/".STATUSNET_VERSION." (".STATUSNET_CODENAME.")";
+        return "GNU Social/".STATUSNET_VERSION." (".STATUSNET_CODENAME.")";
     }
 
     /**
      * Actually performs the HTTP request and returns a
-     * StatusNet_HTTPResponse object with response body and header info.
+     * GNUSocial_HTTPResponse object with response body and header info.
      *
      * Wraps around parent send() to add logging and redirection processing.
      *
-     * @return StatusNet_HTTPResponse
+     * @return GNUSocial_HTTPResponse
      * @throw HTTP_Request2_Exception
      */
     public function send()
@@ -301,6 +301,6 @@ class HTTPClient extends HTTP_Request2
             }
             break;
         } while ($maxRedirs);
-        return new StatusNet_HTTPResponse($response, $this->getUrl(), $redirs);
+        return new GNUSocial_HTTPResponse($response, $this->getUrl(), $redirs);
     }
 }
