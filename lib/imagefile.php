97812549b99aaae5b94f5f4b538ddc5484c28fdf
Mikael Nordfeldth <mmn@hethane.se>
2015-01-26 01:16:28 +0100

    Unnecessarily spammy logs for getThumbnail()
    When trying to create thumbnails for remote media that don't have the
    filename field set, we got a lot of output in the debug log.

--- lib/imagefile.php
+++ lib/imagefile.php
@@ -110,6 +110,9 @@ class ImageFile
         $imgPath = null;
         $media = common_get_mime_media($file->mimetype);
         if (Event::handle('CreateFileImageThumbnailSource', array($file, &$imgPath, $media))) {
+            if (empty($file->filename)) {
+                throw new UnsupportedMediaException(_('File without filename could not get a thumbnail source.'));
+            }
             switch ($media) {
             case 'image':
                 $imgPath = $file->getPath();
