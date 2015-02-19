0dfe39ac8757c81eb9bf2c6dae7ca409038aa843
Mikael Nordfeldth <mmn@hethane.se>
2015-02-19 18:59:13 +0100

    File_thumbnail url is not an index

--- classes/File_thumbnail.php
+++ classes/File_thumbnail.php
@@ -27,7 +27,7 @@ class File_thumbnail extends Managed_DataObject
 {
     public $__table = 'file_thumbnail';                  // table name
     public $file_id;                         // int(4)  primary_key not_null
-    public $url;                             // varchar(255)  unique_key
+    public $url;                             // text
     public $filename;                        // varchar(255)
     public $width;                           // int(4)  primary_key
     public $height;                          // int(4)  primary_key
@@ -38,7 +38,7 @@ class File_thumbnail extends Managed_DataObject
         return array(
             'fields' => array(
                 'file_id' => array('type' => 'int', 'not null' => true, 'description' => 'thumbnail for what URL/file'),
-                'url' => array('type' => 'varchar', 'length' => 255, 'description' => 'URL of thumbnail'),
+                'url' => array('type' => 'text', 'description' => 'URL of thumbnail'),
                 'filename' => array('type' => 'varchar', 'length' => 255, 'description' => 'if stored locally, filename is put here'),
                 'width' => array('type' => 'int', 'description' => 'width of thumbnail'),
                 'height' => array('type' => 'int', 'description' => 'height of thumbnail'),
