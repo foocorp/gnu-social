20592f184d759c3e6f68165ea7b94a330853eaef
Mikael Nordfeldth <mmn@hethane.se>
2015-12-14 21:44:59 +0100

    getID() will complain loudly if something is wrong
    (just ->id would not)

--- classes/Profile.php
+++ classes/Profile.php
@@ -1242,8 +1242,8 @@ class Profile extends Managed_DataObject
     {
         // XXX: not really a pkey, but should work
 
-        $notice = Notice::pkeyGet(array('profile_id' => $this->id,
-                                        'repeat_of' => $notice->id,
+        $notice = Notice::pkeyGet(array('profile_id' => $this->getID(),
+                                        'repeat_of' => $notice->getID(),
                                         'verb' => ActivityVerb::SHARE));
 
         return !empty($notice);
