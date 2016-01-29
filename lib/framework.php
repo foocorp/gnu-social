43abfe659b9e59296fb90cbc6a7dc8ac4c9ce41a
Mikael Nordfeldth <mmn@hethane.se>
2016-01-30 00:04:18 +0100

    Bump beta number to 4
    We have better webfinger @mention@capability.example at least and
    OpportunisticQM is somewhat refined.

--- lib/framework.php
+++ lib/framework.php
@@ -23,7 +23,7 @@ define('GNUSOCIAL_ENGINE', 'GNU social');
 define('GNUSOCIAL_ENGINE_URL', 'https://www.gnu.org/software/social/');
 
 define('GNUSOCIAL_BASE_VERSION', '1.2.0');
-define('GNUSOCIAL_LIFECYCLE', 'beta3'); // 'dev', 'alpha[0-9]+', 'beta[0-9]+', 'rc[0-9]+', 'release'
+define('GNUSOCIAL_LIFECYCLE', 'beta4'); // 'dev', 'alpha[0-9]+', 'beta[0-9]+', 'rc[0-9]+', 'release'
 
 define('GNUSOCIAL_VERSION', GNUSOCIAL_BASE_VERSION . '-' . GNUSOCIAL_LIFECYCLE);
 
