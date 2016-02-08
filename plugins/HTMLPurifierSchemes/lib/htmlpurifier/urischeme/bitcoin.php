eaa394ed7d4cd34cca511f8066863b5534ca8367
Mikael Nordfeldth <mmn@hethane.se>
2016-02-08 20:20:31 +0100

    bitcoin schema for HTMLPurifier

---
+++ plugins/HTMLPurifierSchemes/lib/htmlpurifier/urischeme/bitcoin.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Validates bitcoin (Bitcoin addresses)
+ * @todo validate address length and stuff
+ */
+
+class HTMLPurifier_URIScheme_bitcoin extends HTMLPurifier_URIScheme
+{
+    /**
+     * @type bool
+     */
+    public $browsable = false;
+
+    /**
+     * @type bool
+     */
+    public $may_omit_host = true;
+
+    /**
+     * @param HTMLPurifier_URI $uri
+     * @param HTMLPurifier_Config $config
+     * @param HTMLPurifier_Context $context
+     * @return bool
+     */
+    public function doValidate(&$uri, $config, $context)
+    {
+        $uri->userinfo = null;
+        $uri->host     = null;
+        $uri->port     = null;
+        return true;
+    }
+}
+
+// vim: et sw=4 sts=4
