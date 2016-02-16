d2507a6266e14376e24cbd8434a5cbc7f6a00b19
Mikael Nordfeldth <mmn@hethane.se>
2016-02-16 02:24:38 +0100

    Gotta declare FullNoticeStream as abstract class

--- lib/fullnoticestream.php
+++ lib/fullnoticestream.php
@@ -5,7 +5,7 @@ if (!defined('GNUSOCIAL')) { exit(1); }
 /**
  * Class for notice streams that does not filter anything out.
  */
-class FullNoticeStream extends NoticeStream
+abstract class FullNoticeStream extends NoticeStream
 {
     protected $selectVerbs = [];
 }
