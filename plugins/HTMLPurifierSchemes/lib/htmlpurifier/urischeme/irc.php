e64c3a1d87d7747c0afc136dcabbdd579cc694ea
Mikael Nordfeldth <mmn@hethane.se>
2016-03-21 02:46:28 +0100

    irc and ircs schemes for HTMLPurifier

---
+++ plugins/HTMLPurifierSchemes/lib/htmlpurifier/urischeme/irc.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Validates irc
+ */
+class HTMLPurifier_URIScheme_irc extends HTMLPurifier_URIScheme
+{
+    /**
+     * @type int
+     */
+    public $default_port = 6667;
+
+    /**
+     * @type bool
+     */
+    public $browsable = false;
+
+    /**
+     * @type bool
+     */
+    public $hierarchical = true;
+
+    /**
+     * @param HTMLPurifier_URI $uri
+     * @param HTMLPurifier_Config $config
+     * @param HTMLPurifier_Context $context
+     * @return bool
+     */
+    public function doValidate(&$uri, $config, $context)
+    {
+        $uri->userinfo = null;
+        return true;
+    }
+}
+
+// vim: et sw=4 sts=4
