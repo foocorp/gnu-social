547f92de070f425b9b0415c7aa87b173c630a2b0
Mikael Nordfeldth <mmn@hethane.se>
2016-04-01 06:51:19 +0200

    Don't fail deleteRelated on NoProfileException

--- lib/activityhandlerplugin.php
+++ lib/activityhandlerplugin.php
@@ -279,6 +279,10 @@ abstract class ActivityHandlerPlugin extends Plugin
         if ($this->isMyNotice($notice)) {
             try {
                 $this->deleteRelated($notice);
+            } catch (NoProfileException $e) {
+                // we failed because of database lookup failure, Notice has no recognized profile as creator
+                // so we skip this. If we want to remove missing notices we should do a SQL constraints check
+                // in the affected plugin.
             } catch (AlreadyFulfilledException $e) {
                 // Nothing to see here, it's obviously already gone...
             }
